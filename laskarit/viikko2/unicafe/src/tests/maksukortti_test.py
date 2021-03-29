c116fd83b61ac860c4acf64d3cbb1ec91ecdf13b
meri saynatkari <meri.saynatkari@gmail.com>
2021-03-29 22:30:03 +0300

    testeja testeja

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,10 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def rahan_lataaminen_kasvattaa_saldoa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "saldo: 10.1")
