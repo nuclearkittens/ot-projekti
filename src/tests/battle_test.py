5abcf6f5848d32f47d6713a9401d4b4ba7eae217
meri saynatkari <meri.saynatkari@gmail.com>
2021-04-13 19:14:33 +0300

    troubleshooting mostly

--- src/tests/battle_test.py
+++ src/tests/battle_test.py
@@ -1,4 +1,7 @@
-class Keys:
+import pygame
+import unittest
+
+class StubKeys:
     def __init__(self):
         self.UP_K, self.DOWN_K = False, False
         self.RIGHT_K, self.LEFT_K = False, False
@@ -6,8 +9,5 @@ class Keys:
         self.BACK_K, self.PAUSE_K = False, False
 
     def reset_keys(self):
-        self.UP_K, self.DOWN_K = False, False
-        self.RIGHT_K, self.LEFT_K = False, False
-        self.SELECT_K, self.START_K = False, False
-        self.BACK_K, self.PAUSE_K = False, False
+        pass
 
